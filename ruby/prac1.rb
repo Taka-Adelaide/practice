61efa1176fc758e16328c9085315437e9198562c
Taka-Adelaide <takapenang08@gmail.com>
2022-01-05 15:25:56 +0900

    fizzbuzz

--- ruby/prac1.rb
+++ ruby/prac1.rb
@@ -1,2 +1,13 @@
-test = 1 + 3
-puts test
+def fizzbuzz(n)
+  if n % 3 == 0 && n % 5 == 0
+    "FizzBuzz"
+  elsif n % 5 == 0
+    "Buzz"
+  elsif n % 3 == 0
+    "Fizz"
+  else
+    puts "none"
+  end
+end
+
+puts fizzbuzz(8)
