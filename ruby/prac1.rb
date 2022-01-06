25e9a8d97bd7876908e8bf321d9d7d5cb33c0070
Taka-Adelaide <takapenang08@gmail.com>
2022-01-06 23:08:31 +0900

    Jan 6th

--- ruby/prac1.rb
+++ ruby/prac1.rb
@@ -1,27 +1,12 @@
-def greeting(country)
-  if country == "jpn"
+def greeting(country = "jp")
+  if country == "jp"
     "こんにちは"
-  elsif country == "chi"
+  elsif country == "ch"
     "你好"
   else
     "Hello"
   end
 end
 
-def welcoming(country)
-  if country == "jpn"
-    "ようこそ"
-  elsif country == "chi"
-    "清在"
-  else
-    "welcome"
-  end
-end
-
-greeting = greeting("jpn")
-welcoming = welcoming("jpn")
-name = "Taro"
-
-message = "#{name}, #{greeting}#{welcoming}"
-
-puts message
+puts greeting
+puts greeting("us")
