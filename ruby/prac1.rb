7faf168b9d84fdcbdbd077623e2fd6f161123229
Taka-Adelaide <takapenang08@gmail.com>
2022-01-08 20:15:19 +0900

    0108

--- ruby/prac1.rb
+++ ruby/prac1.rb
@@ -1,32 +1,8 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    "FizzBuzz"
-  elsif n % 5 == 0
-    "Buzz"
-  elsif n % 3 == 0
-    "Fizz"
-  else
-    n.to_s
-  end
-end
+numbers = [1,2,3,4,5]
+# new_num = []
+# numbers.each {|n| new_num << n * 10 }
 
-# require 'minitest/autorun'
+new_num = numbers.map {|n| n * 10}
 
-# class FizzBuzzTest < Minitest::Test
-#   def test_fizz_buzz
-#     assert_equal '1', fizz_buzz(1)
-#     assert_equal '2', fizz_buzz(2)
-#     assert_equal 'Fizz', fizz_buzz(3)
-#     assert_equal '4', fizz_buzz(4)
-#     assert_equal 'Buzz', fizz_buzz(5)
-#     assert_equal 'Fizz', fizz_buzz(6)
-#     assert_equal 'FizzBuzz', fizz_buzz(30)
-#   end
-# end
-
-
-
-# puts fizz_buzz(30)
-# puts fizz_buzz(20)
-# puts fizz_buzz(21)
-# puts fizz_buzz(2)
+puts new_num
+print new_num
