9cefb939e2b1cc7e7dd36bf09d54a3b6787c34b7
Taka-Adelaide <takapenang08@gmail.com>
2022-01-10 20:27:08 +0900

    check

--- ruby/prac1.rb
+++ ruby/prac1.rb
@@ -1,47 +1,9 @@
-# def charge(age)
-#   case age
-#   when 0..5
-#     0
-#   when 6..12
-#     300
-#   when 13..18
-#     600
-#   else
-#     1000
-#   end
-# end
-
-# def charge(age)
-#   if 0 <= age && age <= 5
-#     0
-#   elsif 6 <= age && age <= 12
-#     300
-#   elsif 13 <= age && age <= 18
-#     600
-#   else 
-#     1000
-#   end
-# end
-
-# charge = 
-#   (1...5).to_a
+fruits = ['apple', 'orange', 'melon']
 
 
-# puts charge
-# puts charge(18)
+  fruits.each.with_index(100) do |fruit, i|
+    a = "#{i}: #{fruit}"
+    puts a
+  end
 
-# a = 0.to_s(16).rjust(10, '0')
 # puts a
-
-# hex = "#12abcd"
-# r = hex[1..2]
-# g = hex[3..4]
-# b = hex[5..6]
-
-r = '00'.hex
-g = 'ff'.hex
-b = '2a'.hex
-
-puts r
-puts g
-puts b
