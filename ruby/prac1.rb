febe191296275ebc672b66fdb8ec095572487400
Taka-Adelaide <takapenang08@gmail.com>
2022-01-17 16:58:09 +0900

    ruby Prac

--- ruby/prac1.rb
+++ ruby/prac1.rb
@@ -1,7 +1,26 @@
-text = <<TEXT
-I love Ruby.
-Python is a great language.
-Java and JavaScript are different language.
-TEXT
+class User
+  attr_reader :first_name, :last_name, :age
 
-puts text.scan(/[A-Z][A-Za-z]+/)
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
+
+users = []
+users << User.new('Alice', 'Ruby', 20)
+users << User.new('Bob', 'Python', 30)
+
+# def full_name(user)
+#   "#{user.first_name} #{user.last_name}"
+# end
+
+users.each do |user|
+  puts "氏名: #{user.full_name}、年齢: #{user.age}"
+end
+
