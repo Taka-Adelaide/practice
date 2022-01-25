076cc536bd106274a52d03980707fb3e2d7303c6
Taka-Adelaide <takapenang08@gmail.com>
2022-01-25 21:58:55 +0900

    module

--- ruby/prac1.rb
+++ ruby/prac1.rb
@@ -1,64 +1,16 @@
-# class Product
-#   def title
-#     log 'title is called.'
-#     'Great Movie'
-#   end
-
-#   private
-
-#   def log(text)
-#     puts "[LOG] #{text}"
-#   end
-# end
-
-# class User
-#   def name
-#     log 'name is called.'
-#     'Alice'
-#   end
-
-#   private
-
-#   def log(text)
-#     puts "[LOG] #{text}"
-#   end
-# end
-
-# product = Product.new
-# puts product.title
-
-# user = User.new
-# puts user.name
-
-module Loggable
-
-  private
-  
-  def log(text)
-    puts "[LOG] #{text}"
+module Taggable
+  def price_tag
+    "#{price}円"
   end
 end
 
 class Product
-  include Loggable
+  include Taggable
 
-  def title
-    log 'title is called.'
-    'Great movie'
-  end
-end
-
-class User
-  include Loggable
-
-  def name
-    log 'name is called.'
-    'Alice'
+  def price
+    1000
   end
 end
 
 product = Product.new
-puts product.title
-
-user = User.new
-puts user.name
+puts product.price_tag
