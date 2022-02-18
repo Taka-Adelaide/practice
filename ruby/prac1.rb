f23bdeb8cae446955e9eae7a040daa36b9bbc3de
Taka-Adelaide <takapenang08@gmail.com>
2022-02-18 18:01:45 +0900

    practice

---
+++ ruby/prac1.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(n)
+  if n % 15 == 0
+    "FizzBuzz"
+  elsif n % 5 == 0
+    "Buzz"
+  elsif n % 3 == 0
+    "Fizz"
+  else
+    n.to_s
+  end
+end
+
+puts fizzbuzz(30)
+puts fizzbuzz(20)
+puts fizzbuzz(9)
+puts fizzbuzz(4)
+
