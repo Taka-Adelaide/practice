5c755e39cdaf49c4507fbcfe10de929361169b12
Taka-Adelaide <takapenang08@gmail.com>
2022-03-02 14:55:45 +0900

    practice

--- java/practice.java
+++ java/practice.java
@@ -1,26 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    // 変数nameに、好きな名前を代入してください
-    String name = "Taro";
+    // 変数numbersに、与えられた数字の配列を代入してください
+    int numbers[] = {1, 4, 6, 9, 13, 16};
     
-    // 変数ageに、好きな年齢を代入してください
-    int age = 10;
+    int oddSum = 0;
+    int evenSum = 0;
     
-    // 変数heightに、好きな身長を代入してください（例: 1.2）
-    double height = 1.5;
-    
-    // 変数weightに、好きな体重を代入してください（例: 20.0）
-    double weight = 40.5;
-    
-    // 変数bmiに、BMIを計算して代入してください
-    double bmi = weight / height / height;
-    
-    // 名前、年齢、身長、体重、BMIに関する情報を出力してください
-    System.out.println("名前は" + name + "です");
-    System.out.println("年齢は" + age + "歳です");
-    System.out.println("身長は" + height + "mです");
-    System.out.println("体重は" + weight + "kgです");
-    System.out.println("BMIは" + bmi + "です");
-    
+    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
+    for(int number: numbers) {
+      if(number % 2 == 0) {
+        evenSum += number;
+      } else {
+        oddSum += number;
+      }
+    }
+
+    System.out.println("奇数の和は" + oddSum + "です");
+    System.out.println("偶数の和は" + evenSum + "です");
   }
 }
