8ad1bb853e32802b166a753ef8910c0ef13ba4b3
Taka-Adelaide <takapenang08@gmail.com>
2022-03-03 18:06:15 +0900

    prac

--- java/practice.java
+++ java/practice.java
@@ -1,21 +1,92 @@
 class Main {
   public static void main(String[] args) {
-    // 変数numbersに、与えられた数字の配列を代入してください
-    int numbers[] = {1, 4, 6, 9, 13, 16};
+    // 仕事を「医者」にしてください
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0, "医者");
+    person1.printData();
+    // 仕事を「教師」にしてください
+    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0, "教師");
+    person2.printData();
+    System.out.println("----------------------");
+    // person1の仕事を「獣医」に変更してください
+    person1.setJob("獣医");
     
-    int oddSum = 0;
-    int evenSum = 0;
+    // 「person1の仕事を◯◯に変更しました」と出力されるようにしてください
+    System.out.println("person1の仕事を" + person1.getJob() + "に変更しました");
     
-    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
-    for(int number: numbers) {
-      if(number % 2 == 0) {
-        evenSum += number;
-      } else {
-        oddSum += number;
-      }
-    }
+    person1.printData();
+  }
+}
 
-    System.out.println("奇数の和は" + oddSum + "です");
-    System.out.println("偶数の和は" + evenSum + "です");
+class Person {
+  private static int count = 0;
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private int age;
+  private double height;
+  private double weight;
+  // インスタンスフィールド「job」を追加してください
+  private String job;
+  
+  // コンストラクタを書き換えてください
+  Person(String firstName, String lastName, int age, double height, double weight, String job) {
+    Person.count++;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.job = job;
+  }
+  
+  // コンストラクタを書き換えてください
+  Person(String firstName, String middleName, String lastName, int age, double height, double weight, String job) {
+    this(firstName, lastName, age, height, weight, job);
+    this.middleName = middleName;
+  }
+
+  public String getMiddleName() {
+    return this.middleName;
+  }
+  
+  // jobのゲッターを定義してください
+  public String getJob() {
+    return this.job;
+  }
+
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+  
+  // jobのセッターを定義してください
+  public void setJob(String job) {
+    this.job = job;
+  }
+  
+
+  public String fullName() {
+    if (this.middleName == null) {
+      return this.firstName + " " + this.lastName;
+    } else {
+      return this.firstName + " " + this.middleName + " " + this.lastName;
+    }
+  }
+
+  public void printData() {
+    System.out.println("私の名前は" + this.fullName() + "です");
+    System.out.println("年齢は" + this.age + "歳です");
+    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
+    // 「仕事は◯◯です」と出力してください
+    System.out.println("仕事は" + this.job + "です");
+    
+  }
+
+  public double bmi() {
+    return this.weight / this.height / this.height;
+  }
+
+  public static void printCount() {
+    System.out.println("合計" + Person.count + "人です");
   }
 }
