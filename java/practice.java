a9557b50260f04593f4f181278def69782244bd9
Taka-Adelaide <takapenang08@gmail.com>
2022-03-08 17:16:34 +0900

    practice!

--- java/practice.java
+++ java/practice.java
@@ -163,5 +163,14 @@ class Car extends Vehicle {
   }
 }
 
+class Bicycle extends Vehicle {
+  Bicycle(String name, String color) {
+    super(name, color);
+  }
 
+  public void run(int distance) {
+    System.out.println(distance + "km走ります");
+    this.distance += distance;
+    System.out.println("走行距離：" + this.distance + "km");
+  }
 }
