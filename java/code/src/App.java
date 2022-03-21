c65b4b2b2a3d5d4eedadf29301ecf8eee9ca84bc
Taka-Adelaide <takapenang08@gmail.com>
2022-03-21 17:56:24 +0900

    市松模様

--- java/code/src/App.java
+++ java/code/src/App.java
@@ -1,12 +1,38 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int row = 5;
-        int col = 7;
+        int t = 6;
+        int y = 8;
+        
+        // for(int i = 0; i < t; i++) {
+        //     int x = i % 2;
 
-        for(int i = 0; i < row; i++){
-            if(i % 2 == 0){
-                for(int j = 0; j < col; j++){
-                    System.out.print('☆');
+        //     for(int j = 0; j < y; j++) {
+        //         if(j % 2 == x) {
+        //             System.out.print('☆');
+        //         } else {
+        //             System.out.print('　');
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        for(int q = 0; q < t; q++){
+            if(q % 2 == 0) {
+                for(int i = 0; i < y; i++) {
+                    if(i % 2 == 0) {
+                        System.out.print('〇');
+                    } else {
+                        System.out.print('　');
+                    }
+                }
+            } else {
+                for(int j = 0; j < y; j++) {
+                    if(j % 2 == 0) {
+                        System.out.print('　');
+                    } else {
+                        System.out.print('〇');
+                    }
                 }
             }
             System.out.println();
