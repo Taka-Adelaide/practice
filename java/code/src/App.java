eee99e44d0c8726136e1c3d35a567b39e720c362
Taka-Adelaide <takapenang08@gmail.com>
2022-03-22 18:37:21 +0900

    prac

--- java/code/src/App.java
+++ java/code/src/App.java
@@ -1,38 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int t = 6;
-        int y = 8;
-        
-        // for(int i = 0; i < t; i++) {
-        //     int x = i % 2;
+        int n = 8;
 
-        //     for(int j = 0; j < y; j++) {
-        //         if(j % 2 == x) {
-        //             System.out.print('☆');
-        //         } else {
-        //             System.out.print('　');
-        //         }
-        //     }
-        //     System.out.println();
-        // }
-
-
-        for(int q = 0; q < t; q++){
-            if(q % 2 == 0) {
-                for(int i = 0; i < y; i++) {
-                    if(i % 2 == 0) {
-                        System.out.print('〇');
-                    } else {
-                        System.out.print('　');
-                    }
-                }
-            } else {
-                for(int j = 0; j < y; j++) {
-                    if(j % 2 == 0) {
-                        System.out.print('　');
-                    } else {
-                        System.out.print('〇');
-                    }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == n - i) {
+                    System.out.print('☆');
+                    break;
+                } else {
+                    System.out.print('　');
                 }
             }
             System.out.println();
