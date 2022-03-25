1bc382ed9ecf624c4fd07d88ba1effc7ee295a94
Taka-Adelaide <takapenang08@gmail.com>
2022-03-25 18:20:55 +0900

    prac

--- java/code/src/App.java
+++ java/code/src/App.java
@@ -1,9 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int n = 7;
-        
-        for (int i = 0; i <= n; i++) {
-            System.out.print('☆');
+        int n = 10;
+
+        if (n > 10) {
+            System.out.println("11以上は無効です");
+        } else {
+            for (int i = 0; i <= n; i++) {
+                if (i % 2 == 0) {
+                    continue;
+                }
+                System.out.println(i);
+            }
         }
     }
 }
--- python/practice.py
+++
@@ -1,9 +0,0 @@
-# n = 4
-
-def odd(n):
-  if n % 2 == 0:
-    return 'even'
-  else:
-    return 'odd'
-
-print(odd(9))
