44c75348938d1086d0d5852e46f07a17671cffe3
Taka-Adelaide <takapenang08@gmail.com>
2022-05-15 12:15:40 +0900

    復習

--- java/code/src/App.java
+++ java/code/src/App.java
@@ -1,16 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int n = 10;
+        // int n = 9;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int t = 1; i <= n; t++) {
+        //         System.out.println(t * i);
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
 
-        if (n > 10) {
-            System.out.println("11以上は無効です");
-        } else {
-            for (int i = 0; i <= n; i++) {
-                if (i % 2 == 0) {
-                    continue;
-                }
-                System.out.println(i);
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) { // iの段を表示する
+                int ans = i * j;
+                System.out.print(ans);
+                System.out.print(' '); // 半角スペースで区切る
             }
+            System.out.println(); // 行（iの段）が終われば改行
         }
     }
 }
